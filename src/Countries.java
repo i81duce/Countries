ae7755e905b68aded6076cb541c8f482f5a1d94b
KevinBozic <bozic.kevin@gmail.com>
2016-02-11 15:02:44 -0500

    Added arrayList

--- src/Countries.java
+++ src/Countries.java
@@ -1,11 +1,37 @@
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+import java.util.StringJoiner;
 
 public class Countries {
-    public static void main(String[] args) {
+    public static Scanner input = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        HashMap<String, ArrayList<Country>> countriesMap = new HashMap<>();
+        ArrayList<Country> countryList = new ArrayList<>();
 
-    }
+        File f = new File("countries.txt");
+        Scanner fileScanner = new Scanner(f);
+        while (fileScanner.hasNext()) {
+            String line = fileScanner.nextLine();
+            String[] columns = line.split("\\|");
+            Country country = new Country(columns[1], columns[0]);
+            countryList.add(country);
+        }
+        System.out.println("Please type a letter:");
+        String userInput = input.nextLine();
+        String fileName = String.format("%s_countries.txt", userInput.toUpperCase());
+        String fileContent = "";
+        for (Country temp : countryList) {
+           if (temp.name.startsWith(userInput)) {
+               fileContent += temp.name+"\n";
+           }
+        }
+        writeFile(fileName, fileContent);
+
+
+    } //end main
+
 
     static void writeFile(String fileName, String fileContent) throws IOException {
         File f = new File(fileName);
@@ -13,4 +39,5 @@ public class Countries {
         fw.write(fileContent);
         fw.close();
     }
-}
+
+}//end class
---
+++ src/Country.java
@@ -0,0 +1,9 @@
+public class Country {
+    String name;
+    String abbr;
+
+    public Country(String name, String abbr) {
+        this.name = name;
+        this.abbr = abbr;
+    }
+}
